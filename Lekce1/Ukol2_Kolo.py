a65ba1512d38a5152a3c06302700301535fab94d
olesya <olesya.vegrenovskaya@gmail.com>
2021-10-03 17:40:43 +0200

    Add ex.2_Kolo update "Rozšířené zadání"

--- Lekce1/Ukol2_Kolo.py
+++ Lekce1/Ukol2_Kolo.py
@@ -10,6 +10,9 @@ print(df_London_bicycle_rental.columns)
 print(df_London_bicycle_rental)
 df_London_bicycle_rental["timestamp"] = pandas.to_datetime(df_London_bicycle_rental["timestamp"])
 df_London_bicycle_rental["year"] = df_London_bicycle_rental["timestamp"].dt.year
-df_London_bicycle_rental_pivot = pandas.pivot_table(df_London_bicycle_rental, index="year", columns="weather_code", values="cnt", aggfunc=numpy.sum)
+df_London_bicycle_rental_pivot = pandas.pivot_table(df_London_bicycle_rental, index="year", columns="weather_code", values="cnt", aggfunc=numpy.sum, margins=True)
 
 print(df_London_bicycle_rental_pivot)
+
+df_London_bicycle_rental_pivot2 = df_London_bicycle_rental_pivot.div(df_London_bicycle_rental_pivot.iloc[:,-1], axis=0)
+print(df_London_bicycle_rental_pivot2)
