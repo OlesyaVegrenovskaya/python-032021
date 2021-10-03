e2d77ef1d5fa893bdbace48394a6e3f794d7eff8
olesya <olesya.vegrenovskaya@gmail.com>
2021-10-03 03:04:44 +0200

    Add ex.2_London_bike

---
+++ Lekce1/Ukol2_Kolo.py
@@ -0,0 +1,15 @@
+import requests
+import pandas
+import numpy
+
+r = requests.get("https://raw.githubusercontent.com/pesikj/progr2-python/master/data/london_merged.csv")
+open("../london_merged.csv", 'wb').write(r.content)
+
+df_London_bicycle_rental = pandas.read_csv("london_merged.csv")
+print(df_London_bicycle_rental.columns)
+print(df_London_bicycle_rental)
+df_London_bicycle_rental["timestamp"] = pandas.to_datetime(df_London_bicycle_rental["timestamp"])
+df_London_bicycle_rental["year"] = df_London_bicycle_rental["timestamp"].dt.year
+df_London_bicycle_rental_pivot = pandas.pivot_table(df_London_bicycle_rental, index="year", columns="weather_code", values="cnt", aggfunc=numpy.sum)
+
+print(df_London_bicycle_rental_pivot)
